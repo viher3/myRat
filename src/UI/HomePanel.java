4a792e6cb6d7fb1d2f673370b7a55b7f43d88d7f
alberto.vian <alberto.vian@digimobil.es>
2021-07-07 17:54:07 +0200

    Status tab init

--- src/UI/HomePanel.java
+++ src/UI/HomePanel.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class HomePanel {
     public JPanel window;
+    private JTabbedPane tabbedPane1;
+    private JTextArea logTextarea;
 
     private void createUIComponents() {
 
