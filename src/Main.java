db69819559154fe4f63944a09c9058c1b8a65be7
alberto.vian <alberto.vian@digimobil.es>
2021-07-07 17:44:12 +0200

    main jframe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import UI.HomePanel;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World !!!");
+        JFrame frame = new JFrame("myRat");
+        frame.setContentPane(new HomePanel().window);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 600);
+
+        //Get the screen size
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+
+        //Calculate the frame location
+        int x = (screenSize.width - frame.getWidth()) / 2;
+        int y = (screenSize.height - frame.getHeight()) / 2;
+
+        //Set the new frame location
+        frame.setLocation(x, y);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/UI/HomePanel.java
@@ -0,0 +1,11 @@
+package UI;
+
+import javax.swing.*;
+
+public class HomePanel {
+    public JPanel window;
+
+    private void createUIComponents() {
+
+    }
+}
